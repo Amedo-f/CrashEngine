3d2ecf85b8fb9342f1da6ea14329a7ee2c448ca4
amedo <ahmedalzhrani151@gmail.com>
2026-09-21 01:18:50 +0300

    New Scene: strip only positional lights, keep general scene lighting

--- CrashEngine.Importer/BlankSceneTemplate.cs
+++ CrashEngine.Importer/BlankSceneTemplate.cs
@@ -184,11 +184,9 @@ public static class BlankSceneTemplate
         {
             scenery.Name = sceneName;
             scenery.Sceneries.Clear();
-            // Amedo 2026-09-20
+            // Amedo 2026-09-21
             scenery.PointLights.Clear();
-            scenery.AmbientLights.Clear();
-            scenery.DirectionalLights.Clear();
-            scenery.HasLighting = false;
+            scenery.NegativeLights.Clear();
         }
 
         var collision = rm2.GetItem<PS2AnyCollisionData>((uint)Constants.LEVEL_COLLISION_ITEM);
